ccba62d09214bcaafca66654afaf32e46ac0b22b
duynguyen1311 <duy0256@gmail.com>
2024-09-23 22:33:36 +0700

    Section 4 : passing property through components , creating search bar which search on every changes

--- src/components/MovieGrid.js
+++ src/components/MovieGrid.js
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from "react";
 import '../styles.css';
-
+import MovieCard from "./MovieCard";
 export default function MovieGrid(){
 
-    const [movies, setMovies] = useState([])
-
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
     useEffect(() => {
         fetch("movies.json")
             .then(response => response.json())
@@ -12,20 +12,28 @@ export default function MovieGrid(){
             .catch(error => console.log(error))
     }, []);
 
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value)
+    };
+    const filteredMovies = movies.filter(movie =>
+        movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+    )
     return (
-        <div className='movies-grid'>
-            {
-                movies.map(movie => (
-                    <div key={movie.id} className='movie-card'>
-                        <img src={`images/${movie.image}`} alt={movie.title} />
-                        <div className='movie-card-info'>
-                            <h3 className='movie-card-title'>{movie.title}</h3>
-                            <p className='movie-card-genre'>{movie.genre}</p>
-                            <p className='movie-card-rating'>{movie.rating}</p>
-                        </div>
-                    </div>
-                ))
-            }
+        <div>
+            <input
+                className='search-input'
+                type='text'
+                placeholder='Search movies ...'
+                value={searchTerm}
+                onChange={handleSearchChange}
+            />
+            <div className='movies-grid'>
+                {
+                    filteredMovies.map(movie => (
+                        <MovieCard movie={movie} key={movie.id}></MovieCard>
+                    ))
+                }
+            </div>
         </div>
     )
 }
