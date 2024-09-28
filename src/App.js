bd311eb879be79880b9264df7863118a01151f4c
duynguyen1311 <duy0256@gmail.com>
2024-09-28 15:12:21 +0700

    Finish Moviedux project

--- src/App.js
+++ src/App.js
@@ -4,16 +4,73 @@ import './styles.css';
 import Header from './components/Header'
 import Footer from "./components/Footer";
 import MovieGrid from "./components/MovieGrid";
+import Watchlist from "./components/Watchlist";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import {useEffect, useState} from "react";
+
 function App() {
-  return (
-    <div className="App">
-        <div className='container'>
-            <Header></Header>
-            <MovieGrid></MovieGrid>
+    const [movies, setMovies] = useState([]);
+    const [watchlist, setWatchlist] = useState([]);
+
+    useEffect(() => {
+        fetch("movies.json")
+            .then((response) => response.json())
+            .then((data) => setMovies(data));
+    }, []);
+
+    const toggleWatchlist = (movieId) => {
+        setWatchlist((prev) =>
+            prev.includes(movieId)
+                ? prev.filter((id) => id !== movieId)
+                : [...prev, movieId]
+        );
+    };
+    console.log(watchlist)
+    return (
+        <div className="App">
+            <div className="container">
+                <Header></Header>
+
+                <Router>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/watchlist">Watchlist</Link>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <MovieGrid
+                                    watchlist={watchlist}
+                                    movies={movies}
+                                    toggleWatchlist={toggleWatchlist}
+                                />
+                            }
+                        ></Route>
+                        <Route
+                            path="/watchlist"
+                            element={
+                                <Watchlist
+                                    watchlist={watchlist}
+                                    movies={movies}
+                                    toggleWatchlist={toggleWatchlist}
+                                />
+                            }
+                        ></Route>
+                    </Routes>
+                </Router>
+            </div>
+
+            <Footer></Footer>
         </div>
-        <Footer></Footer>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/MovieGrid.js
+++ src/components/MovieGrid.js
@@ -1,19 +1,11 @@
 import React, {useState, useEffect} from "react";
 import '../styles.css';
 import MovieCard from "./MovieCard";
-export default function MovieGrid(){
-
-    const [movies, setMovies] = useState([]);
+export default function MovieGrid({movies, watchlist, toggleWatchlist}){
     const [searchTerm, setSearchTerm] = useState("");
     const [genre, setGenre] = useState("All Genres");
     const [rating, setRating] = useState("All");
 
-    useEffect(() => {
-        fetch("movies.json")
-            .then(response => response.json())
-            .then(data => setMovies(data))
-            .catch(error => console.log(error))
-    }, []);
 
     const handleSearchChange = (e) => {
         setSearchTerm(e.target.value)
@@ -81,11 +73,14 @@ export default function MovieGrid(){
                 </div>
             </div>
             <div className='movies-grid'>
-                {
-                    filteredMovies.map(movie => (
-                        <MovieCard movie={movie} key={movie.id}></MovieCard>
-                    ))
-                }
+                {filteredMovies.map((movie) => (
+                        <MovieCard
+                            movie={movie}
+                            key={movie.id}
+                            toggleWatchlist={toggleWatchlist}
+                            isWatchlisted={watchlist.includes(movie.id)}
+                        ></MovieCard>
+                    ))}
             </div>
         </div>
     )
